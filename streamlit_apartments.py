8c84cec239480b2ddeb847806ba9d1a58c5a8370
OmerS16 <omerseter@gmail.com>
2024-11-03 23:47:23 +0200

    Updated files

--- streamlit_apartments.py
+++ streamlit_apartments.py
@@ -34,12 +34,8 @@ m = folium.Map(location=map_center, zoom_start=12)
 for _, row in filtered_apartments.iterrows():
     folium.Marker(
         location=[row['lat'], row['lon']],
-        popup=f"<a href='{row['url']}' target='_blank'>Click here for details</a>",
-        tooltip=row.get('street')
+        # popup=f"<a href='{row['url']}' target='_blank'>Click here for details</a>",
+        # tooltip=row.get('street')
         ).add_to(m)
     
 st_folium(m, width=700, height=500)
-
-if 'first_load' not in st.session_state:
-    st.session_state['first_load'] = True
-    st.experimental_set_query_params(refresh=True)
