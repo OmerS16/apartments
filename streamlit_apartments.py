2687375bc8e379bc9b0d4dd32fe27bdd0a6cf7a5
OmerS16 <omerseter@gmail.com>
2024-12-10 20:17:21 +0200

    Updated files

--- streamlit_apartments.py
+++ streamlit_apartments.py
@@ -16,7 +16,7 @@ st.title("Find the best neighborhood for your budget")
 st.sidebar.header("Input your preferences")
 
 budget = st.sidebar.number_input("Enter your budget (in shekels):", min_value=0, value=5000, step=1000)
-num_rooms = st.sidebar.number_input("Enter the number of rooms:", min_value=1, value=3, step=1)
+num_rooms = st.sidebar.number_input("Enter the number of rooms:", min_value=1, value=2, step=1)
 walking_time = st.sidebar.number_input("Enter the maximum walking distance from light rail stations (in minutes):", min_value=0, value=5, step=1)
 
 filtered_average_price = average_price[(average_price['rooms'] == num_rooms) & (average_price['price_mean'] <= budget)]
