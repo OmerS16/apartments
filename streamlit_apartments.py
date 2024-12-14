44787bc1583914c22f069e28c013959f44cc73b4
OmerS16 <omerseter@gmail.com>
2024-12-14 17:37:35 +0200

    Updated files

--- streamlit_apartments.py
+++ streamlit_apartments.py
@@ -57,8 +57,8 @@ for _, row in filtered_apartments.iterrows():
                     <a href="{row['url']}" target="_blank" style="color: black; text-decoration: none;">
                         <h5><b>{row['street']} {int(row['house_num'])}</b></h5>
                         <h5>₪{row['price']}</h5>
-                        <h5>{int(row['rooms'])} חדרים</h5>
-                        <h5>{int(row['sq_m'])} מ"\ר</h5>
+                        <h6>{int(row['rooms'])} חדרים</h6>
+                        <h6>{int(row['sq_m'])} מ"\ר</h6>
                     </a>
                 </div>
             </div>
