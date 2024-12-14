d77546d349857e40a4445b2a3d7b2c1e62998295
OmerS16 <omerseter@gmail.com>
2024-12-14 17:21:24 +0200

    Updated files

--- streamlit_apartments.py
+++ streamlit_apartments.py
@@ -49,15 +49,12 @@ for _, row in filtered_apartments.iterrows():
         # popup=f"<a href='{row['url']}' target='_blank'>Click here for details</a>",
         popup=f"""
             <div style="display: flex; align-items: center;">
-                <div style="margin-right: 10px; white-space:nowrap;">
-                    <a href="{row['url']}" target="_blank">
-                        <h5><b>{row['street']} {int(row['house_num'])}</b></h5>
-                        </a>
-                    <h5>₪{row['price']}</h5>
-                </div>
                 <a href="{row['url']}" target="_blank">
-                    <img src="{row['image']}" 
-                         style="width:200px; height:150px; object-fit:cover; border-radius:5px;">
+                    <div style="margin-right: 10px; white-space:nowrap;">    
+                        <h5><b>{row['street']} {int(row['house_num'])}</b></h5>
+                        <h5>₪{row['price']}</h5>
+                    </div>
+                    <img src="{row['image']}" style="width:200px; height:150px; object-fit:cover; border-radius:5px;">
                 </a>
             </div>
         """,
